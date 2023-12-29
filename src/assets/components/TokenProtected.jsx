97850fe0c1a87f1707b71274068b0d047e7602e6
gamprat <gamprat.x@gmail.com>
2023-12-30 00:37:17 +0700

    Perubahan Kode New

--- src/assets/components/TokenProtected.jsx
+++ src/assets/components/TokenProtected.jsx
@@ -4,9 +4,6 @@ import { useSelector } from "react-redux";
 import { GetUsersProfileCheck } from "../../services/users/get-user-profile";
 
 function TokenProtected({ children }) {
-  const navigate = useNavigate();
-  const Data = useSelector((state) => state.auth);
-
   const getDataToken = async () => {
     let data = await GetUsersProfileCheck();
     console.log(data, "data");
@@ -15,7 +12,6 @@ function TokenProtected({ children }) {
   useEffect(() => {
     getDataToken();
   }, []);
-
 
   return children;
 }
