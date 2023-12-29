cae045d088da02ba05982a420438029b7b5dda54
Muhammad Fernandes <132972538+NdeZzz05@users.noreply.github.com>
2023-12-29 20:53:10 +0700

    Delete file yang tidak terpakai

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from "redux";
 import getDataCourses from "./get-data-courses";
-import getDataMyEnrollments from "./get-data-my-enrollments";
 
 export default combineReducers({
   getDataCourses: getDataCourses,
-  getDataMyEnrollments: getDataMyEnrollments,
 });
--- src/services/get-data-my-enrollments.js
+++
@@ -1,6 +0,0 @@
-import { API_ENDPOINT } from "../utils/api-endpoint";
-import http from "../utils/http";
-// ini yang pake redux
-export const reduxGetDataMyEnrollments = async () => {
-  return await http.get(`${API_ENDPOINT.MY_ENROLLMENTS}?page=1&limit=100`);
-};
